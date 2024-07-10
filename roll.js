41d4bb164d2f4d21fdd8d4d2f4e2eca20d8ad3d4
Jean van Kasteel <vankasteelj@gmail.com>
2024-07-10 18:24:33 +0200

    simplify code and font for title

--- roll.js
+++ roll.js
@@ -2,15 +2,11 @@ const $die = $('.die')
 const $action = $('.result .reaction')
 const $button = $('.button .randomize')
 const sides = 20
-const initialSide = 1
-const transitionDuration = 500
 const animationDuration  = 1500
-let lastFace = undefined
-let lastAction = undefined
 let timeoutId = undefined
 
 const actions = {
-  cfail: [
+  echecCritique: [
     "Échec critique",
     "Tape de rage contre un mur et pète toi la main",
     "Retiens une larme... Craque. Chiale. Le monde est injuste avec toi",
@@ -19,7 +15,7 @@ const actions = {
     "Vexé, tu essayes de t'en sortir avec les leçons de vie de Jordan Peterson",
     "Accepte la contradiction mais passe ta colère sur le premier truc vulnérable à portée"
   ],
-  fail: [
+  echec: [
     "Échec",
     "N'exprime pas ta colère par des mots mais par des soupirs et des râles réguliers de cachalot en rut",
     "Quitte la pièce en claquant la porte",
@@ -31,7 +27,7 @@ const actions = {
     "\"Non mais c'est bon, j'avais compris\"",
     "Oublie instantanément le mode d'emploi de tous les appareils ménagers et n'essaye surtout pas de comprendre comment ils fonctionnent"
   ],
-  mid: [
+  reussiteMoyenne: [
     "Réussite moyenne",
     "Accuse l'autre d'être désagréable",
     "\"Je ne comprends pas, je suis un mec bien\"",
@@ -39,7 +35,7 @@ const actions = {
     "Explique pourquoi l'autre a mal compris",
     "\"On ne peut plus rien dire\""
   ],
-  success: [
+  reussite: [
     "Réussite",
     "\"Arrête d'être en colère, sois rationnelle un peu !\"",
     "\"T'as tes règles ou quoi ?\"",
@@ -50,7 +46,7 @@ const actions = {
     "\"Je suis un homme, c'est normal que je sois en colère\"",
     "\"J'ai eu une mauvaise journée au travail, tu peux comprendre ?\""
   ],
-  csuccess: [
+  reussiteCritique: [
     "Réussite critique",
     "Excuse-toi, puis ajoute un \"mais\"... et déroule à nouveau ton point de vue problématique",
     "\"Non t’as pas bien compris, je vais répéter en PARLANT PLUS FORT\"",
@@ -59,24 +55,22 @@ const actions = {
 }
 
 const randomFace = () => {
-  const face = Math.floor((Math.random() * sides)) + initialSide
-  lastFace = face == lastFace ? randomFace() : face
-  return lastFace;
+  const face = Math.floor((Math.random() * sides)) + 1
+  return face;
 }
 
-const randomAction = (face) => {
+const randomAction = (face, reroll) => {
   const range = (() => {
-    if (face <= 2) return 'cfail'
-    if (face <= 9) return 'fail'
-    if (face <= 15) return 'mid'
-    if (face <= 18) return 'success'
-    if (face <= 20) return 'csuccess'
+    if (face <= 2) return 'echecCritique'
+    if (face <= 9) return 'echec'
+    if (face <= 15) return 'reussiteMoyenne'
+    if (face <= 18) return 'reussite'
+    if (face <= 20) return 'reussiteCritique'
   })()
   const i = actions[range].length - 1
   const rand = Math.floor((Math.random() * i)) + 1
-  lastAction = rand == lastAction ? randomAction(face) : rand
-  console.log('Jet de dé: la face %s est un(e) %s - La phrase %s est: %s', face, actions[range][0], rand, actions[range][lastAction])
-  return actions[range][lastAction]
+  console.log('Résultat: %s - %s (action %s: %s)', face, actions[range][0].toLowerCase(), rand, actions[range][rand])
+  return actions[range][rand]
 }
 
 const rollTo = (face) => {
@@ -90,16 +84,12 @@ $('.randomize, .die').click(() => {
   clearTimeout(timeoutId)
   
   const roll = randomFace()
-  setTimeout(() => {
-    $action.text(randomAction(roll))
-  }, 500)
 
   timeoutId = setTimeout(() => {
+    $action.text(randomAction(roll))
     $die.removeClass('rolling')
     $button.text('Relancer')
-    setTimeout(() => {
-      $action.fadeIn()
-    }, 300)
+    $action.delay(300).fadeIn()
 
     rollTo(roll)
   }, animationDuration)
