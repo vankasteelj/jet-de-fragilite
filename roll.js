6cd5e6e6b3aff3b0a647d8660b5c9d61b7217c8d
Jean van Kasteel <vankasteelj@gmail.com>
2024-07-10 23:26:12 +0200

    reformulate

--- roll.js
+++ roll.js
@@ -20,37 +20,37 @@ const actions = {
     "N'exprime pas ta colère par des mots mais par des soupirs et des râles réguliers de cachalot en rut",
     "Quitte la pièce en claquant la porte",
     "Tu sais que tu as tort. Continue à hurler, admet que tu as tort, puis remets-toi à hurler",
-    "\"Oui mais les hommes souffrent aussi et ça on n'en parle jamais, OK?\"",
+    "Réponds : \"Oui mais les hommes souffrent aussi et ça on n'en parle jamais, OK?\"",
     "Joue la victime",
-    "\"On en parlera quand il s'agira de porter des trucs lourds\"",
-    "\"Non mais je le savais déjà\"",
-    "\"Non mais c'est bon, j'avais compris\"",
+    "Réponds : \"On en parlera quand il s'agira de porter des trucs lourds\"",
+    "Réponds : \"Non mais je le savais déjà\"",
+    "Réponds : \"Non mais c'est bon, j'avais compris\"",
     "Oublie instantanément le mode d'emploi de tous les appareils ménagers et n'essaye surtout pas de comprendre comment ils fonctionnent"
   ],
   reussiteMoyenne: [
     "Réussite moyenne",
     "Accuse l'autre d'être désagréable",
-    "\"Je ne comprends pas, je suis un mec bien\"",
-    "\"C’est bien un truc de gonzesse, ça\"",
+    "Réponds : \"Je ne comprends pas, je suis pourtant un mec bien\"",
+    "Réponds : \"C’est bien un truc de gonzesse, ça\"",
     "Explique pourquoi l'autre a mal compris",
-    "\"On ne peut plus rien dire\""
+    "Réponds : \"On ne peut plus rien dire\""
   ],
   reussite: [
     "Réussite",
-    "\"Arrête d'être en colère, sois rationnelle un peu !\"",
-    "\"T'as tes règles ou quoi ?\"",
-    "\"Je suis désolé que tu n'aies pas compris ce que j'ai voulu dire\"",
-    "\"Je ne pleure pas, je sue de la colère par les yeux\"",
+    "Réponds : \"Arrête d'être en colère, sois rationnelle un peu !\"",
+    "Réponds : \"T'as tes règles ou quoi ?\"",
+    "Réponds : \"Je suis désolé que tu n'aies pas compris ce que j'ai voulu dire\"",
+    "Réponds : \"Je ne pleure pas, je sue de la colère par les yeux\"",
     "Ignore la personne en faisant mine de tâter un mur, avant de sortir : \"C'est un mur porteur ça, non ?\"",
     "Interdiction de te soigner si tu ressens de la douleur. D'ailleurs, tu n'en ressens pas", 
-    "\"Je suis un homme, c'est normal que je sois en colère\"",
-    "\"J'ai eu une mauvaise journée au travail, tu peux comprendre ?\""
+    "Réponds : \"Je suis un homme, c'est normal que je sois en colère\"",
+    "Réponds : \"J'ai eu une mauvaise journée au travail, tu peux comprendre ?\""
   ],
   reussiteCritique: [
     "Réussite critique",
     "Excuse-toi, puis ajoute un \"mais\"... et déroule à nouveau ton point de vue problématique",
-    "\"Non t’as pas bien compris, je vais répéter en PARLANT PLUS FORT\"",
-    "\"Désolé que tu le prennes comme ça\""
+    "Réponds : \"Non t’as pas bien compris, je vais répéter en PARLANT PLUS FORT\"",
+    "Réponds : \"Désolé que tu le prennes comme ça\""
   ]
 }
 
